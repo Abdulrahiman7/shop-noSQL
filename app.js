8176bf955ffd5ef2d349cd6c10b0722b851754a4
Abdulrahiman7 <rahimanabdul981@gmail.com>
2024-06-25 09:44:33 +0530

    changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 const cors=require('cors');
 const bodyParser=require('body-parser');
+const path=require('path');
 
 require('dotenv').config();
 const UserRoute=require('./routes/user.js');
